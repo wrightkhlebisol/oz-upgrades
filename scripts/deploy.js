60d697820e16776e64e11103dbc1dc9592ffcaaa
Wright <calebogundiya@gmail.com>
2021-11-25 23:09:11 +0100

    Create deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+    let Box = await ethers.getContractFactory('Box');
+    console.log("Deploying Box...");
+    let box = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
+    console.log(`Box deployed to: ${box.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(e => {
+        console.error(e);
+        process.exit(1);
+    })
