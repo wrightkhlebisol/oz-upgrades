0d474449d86b3a2273082010f78db89ba2aa4f3d
Wright <calebogundiya@gmail.com>
2021-11-26 09:55:49 +0100

    DOtenv and config hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }
+    }
+  }
 };
